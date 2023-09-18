96d9a61323d286b1152928f47ad75d0ea03de7f0
headshot2017 <>
2023-09-18 14:43:17 -0400

    add main menu to converter tool
    you have the option to convert specific data (backgrounds, or characters, or sounds, etc...) instead of doing all of them in one go

--- converter/conversion.py
+++ converter/conversion.py
@@ -8,140 +8,97 @@ import requests
 
 import images
 
-if os.name == "nt" and not os.path.exists("ffmpeg.exe"):
-    print("Downloading 7zr...")
-    a = requests.get("https://www.7-zip.org/a/7zr.exe")
-    open("7zr.exe", "wb").write(a.content)
-    
-    print("Downloading ffmpeg...")
-    with requests.get("https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z", stream=True) as r:
-        r.raise_for_status()
-        downloaded = 0
-        with open("ffmpeg.7z", 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192): 
-                f.write(chunk)
-                downloaded += len(chunk)
-                print("    %.2f MB / %.2f MB" % (downloaded/1024./1024., int(r.headers["Content-length"])/1024./1024.), end="\r")
-
-    print("Extracting ffmpeg...      ")
-    subprocess.Popen("7zr e ffmpeg.7z -o. ffmpeg.exe -r").wait()
-
-    print("Extracted\n\n")
-    os.remove("ffmpeg.7z")
-    os.remove("7zr.exe")
-
-print("AO NDS converter tool")
-print("This tool will convert your AO 'base' folder to formats that will work with the Nintendo DS.\n")
-
-print("Drag the AO base folder to this console window then press ENTER:")
-folder = input("> ")
-
-try:
-    os.makedirs("converted/data/ao-nds")
-    os.makedirs("converted/data/ao-nds/background")
-    os.makedirs("converted/data/ao-nds/characters")
-    os.makedirs("converted/data/ao-nds/evidence")
-    os.makedirs("converted/data/ao-nds/sounds/general")
-    os.makedirs("converted/data/ao-nds/sounds/music")
-    os.makedirs("converted/data/ao-nds/sounds/blips")
-    os.makedirs("converted/data/ao-nds/misc")
-except:
-    pass
-
-# start
-print("Converting backgrounds...")
-for bg in os.listdir(folder+"/background"):
-    if not os.path.isdir(folder+"/background/"+bg):
-        continue
-
-    print(bg)
-    try: os.mkdir("converted/data/ao-nds/background/"+bg)
-    except: pass
-
-    if os.path.exists("converted/data/ao-nds/background/"+bg+"/desk_tiles.cfg"):
-        os.remove("converted/data/ao-nds/background/"+bg+"/desk_tiles.cfg")
-
-    # convert background first
-    for imgfile in ["defenseempty.png", "prosecutorempty.png", "witnessempty.png", "helperstand.png", "prohelperstand.png", "judgestand.png"]:
-        full_filename = folder+"/background/"+bg+"/"+imgfile
-        if not os.path.exists(full_filename):
+def convertBackgrounds(folder):
+    for bg in os.listdir(folder+"/background"):
+        if not os.path.isdir(folder+"/background/"+bg):
             continue
 
-        no_ext_file = os.path.splitext(imgfile)[0]
+        print(bg)
+        try: os.mkdir("converted/data/ao-nds/background/"+bg)
+        except: pass
 
-        img = Image.open(full_filename).convert("RGB")
-        if img.size[0] != 256 or img.size[1] != 192:
-            img = img.resize((256, 192), Image.BICUBIC)
-        img.save("temp.png")
-        img.close()
+        if os.path.exists("converted/data/ao-nds/background/"+bg+"/desk_tiles.cfg"):
+            os.remove("converted/data/ao-nds/background/"+bg+"/desk_tiles.cfg")
 
-        # 8-bit tiles, generate map file, enable palette, extended palette slot 0, export to .bin, don't generate .h file
-        subprocess.Popen("./grit temp.png -g -gB8 -gt -m -mp 0 -p -ftb -fh!").wait()
+        # convert background first
+        for imgfile in ["defenseempty.png", "prosecutorempty.png", "witnessempty.png", "helperstand.png", "prohelperstand.png", "judgestand.png"]:
+            full_filename = folder+"/background/"+bg+"/"+imgfile
+            if not os.path.exists(full_filename):
+                continue
 
-        if os.path.exists("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".img.bin"):
-            os.remove("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".img.bin")
-        if os.path.exists("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".map.bin"):
-            os.remove("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".map.bin")
-        if os.path.exists("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".pal.bin"):
-            os.remove("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".pal.bin")
-        os.rename("temp.img.bin", "converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".img.bin")
-        os.rename("temp.map.bin", "converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".map.bin")
-        os.rename("temp.pal.bin", "converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".pal.bin")
+            no_ext_file = os.path.splitext(imgfile)[0]
 
-    # then convert desks. we won't bother with helper desks since these are mostly only used for special effects (source: GS4Night background)
-    for imgfile, imgindex in [["defensedesk.png", 0], ["prosecutiondesk.png", 1], ["stand.png", 2], ["judgedesk.png", 3]]:
-        full_filename = folder+"/background/"+bg+"/"+imgfile
-        if not os.path.exists(full_filename):
-            continue
+            img = Image.open(full_filename).convert("RGB")
+            if img.size[0] != 256 or img.size[1] != 192:
+                img = img.resize((256, 192), Image.BICUBIC)
+            img.save("temp.png")
+            img.close()
 
-        no_ext_file = os.path.splitext(imgfile)[0]
+            # 8-bit tiles, generate map file, enable palette, extended palette slot 0, export to .bin, don't generate .h file
+            subprocess.Popen("./grit temp.png -g -gB8 -gt -m -mp 0 -p -ftb -fh!").wait()
 
-        img = Image.open(full_filename).convert("RGBA")
-        if img.size[0] != 256 or img.size[1] != 192:
-            img = img.resize((256, 192), Image.BICUBIC)
+            if os.path.exists("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".img.bin"):
+                os.remove("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".img.bin")
+            if os.path.exists("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".map.bin"):
+                os.remove("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".map.bin")
+            if os.path.exists("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".pal.bin"):
+                os.remove("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".pal.bin")
+            os.rename("temp.img.bin", "converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".img.bin")
+            os.rename("temp.map.bin", "converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".map.bin")
+            os.rename("temp.pal.bin", "converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".pal.bin")
 
-        pix = img.load()
+        # then convert desks. we won't bother with helper desks since these are mostly only used for special effects (source: GS4Night background)
+        for imgfile, imgindex in [["defensedesk.png", 0], ["prosecutiondesk.png", 1], ["stand.png", 2], ["judgedesk.png", 3]]:
+            full_filename = folder+"/background/"+bg+"/"+imgfile
+            if not os.path.exists(full_filename):
+                continue
 
-        # find top image corner from top to bottom til we hit a visible pixel
-        found = False
-        top = 0
-        for y in range(img.size[1]):
-            for x in range(img.size[0]):
-                if pix[x, y][3] != 0:
-                    top = y
-                    found = True
-                    break
-            if found: break
+            no_ext_file = os.path.splitext(imgfile)[0]
 
-        # crop corners
-        img = img.crop((0, top, img.size[0], img.size[1]))
+            img = Image.open(full_filename).convert("RGBA")
+            if img.size[0] != 256 or img.size[1] != 192:
+                img = img.resize((256, 192), Image.BICUBIC)
 
-        # in the AA games on DS, desks are loaded as sprites.
-        # the image width will be set to a size divisible by 64,
-        # and the height will be set to a size divisible by 32
-        # so that it can be loaded as 64x32 tiles
-        horizontalTiles = int(math.ceil(img.size[0]/64.))
-        verticalTiles = int(math.ceil(img.size[1]/32.))
-        img = img.crop((0, img.size[1]-(verticalTiles*32), horizontalTiles*64, img.size[1]))
+            pix = img.load()
 
-        img.save("temp.png")
-        img.close()
-        
-        with open("converted/data/ao-nds/background/"+bg+"/desk_tiles.cfg", "a") as f:
-            f.write("%s: %d,%d\n" % (no_ext_file, horizontalTiles, verticalTiles))
+            # find top image corner from top to bottom til we hit a visible pixel
+            found = False
+            top = 0
+            for y in range(img.size[1]):
+                for x in range(img.size[0]):
+                    if pix[x, y][3] != 0:
+                        top = y
+                        found = True
+                        break
+                if found: break
 
-        # 8-bit tiles, export to .img.bin, don't generate .h file, exclude map data, metatile height and width
-        subprocess.Popen("grit temp.png -gB8 -gt -ftb -fh! -m! -Mh4 -Mw8").wait()
-        
-        if os.path.exists("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".img.bin"):
-            os.remove("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".img.bin")
-        if os.path.exists("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".pal.bin"):
-            os.remove("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".pal.bin")
-        os.rename("temp.img.bin", "converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".img.bin")
-        os.rename("temp.pal.bin", "converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".pal.bin")
+            # crop corners
+            img = img.crop((0, top, img.size[0], img.size[1]))
 
+            # in the AA games on DS, desks are loaded as sprites.
+            # the image width will be set to a size divisible by 64,
+            # and the height will be set to a size divisible by 32
+            # so that it can be loaded as 64x32 tiles
+            horizontalTiles = int(math.ceil(img.size[0]/64.))
+            verticalTiles = int(math.ceil(img.size[1]/32.))
+            img = img.crop((0, img.size[1]-(verticalTiles*32), horizontalTiles*64, img.size[1]))
 
-print("\nConverting characters...")
+            img.save("temp.png")
+            img.close()
+            
+            with open("converted/data/ao-nds/background/"+bg+"/desk_tiles.cfg", "a") as f:
+                f.write("%s: %d,%d\n" % (no_ext_file, horizontalTiles, verticalTiles))
+
+            # 8-bit tiles, export to .img.bin, don't generate .h file, exclude map data, metatile height and width
+            subprocess.Popen("grit temp.png -gB8 -gt -ftb -fh! -m! -Mh4 -Mw8").wait()
+            
+            if os.path.exists("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".img.bin"):
+                os.remove("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".img.bin")
+            if os.path.exists("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".pal.bin"):
+                os.remove("converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".pal.bin")
+            os.rename("temp.img.bin", "converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".img.bin")
+            os.rename("temp.pal.bin", "converted/data/ao-nds/background/"+bg+"/"+no_ext_file+".pal.bin")
+
 
 # frames: [[PIL.Image, ms_duration], [PIL.Image, ms_duration], [PIL.Image, ms_duration]...]
 def convertEmoteFrames(frames, targetFile, ogTarget, extra):
@@ -331,39 +288,29 @@ def convertCharacters(source, target):
 
         recursiveCharacter(source+"/"+char, target+"/"+char, target+"/"+char)
 
-convertCharacters(folder+"/characters", "converted/data/ao-nds/characters")
+def convertEvidenceImages(folder):
+    pass
 
-
-print("\nConverting evidence images...")
-# TO-DO
-
-
-print("\nConverting sounds...")
-def recursiveSounds(source, target):
+def convertSounds(source, target):
     if not os.path.exists(target):
         os.mkdir(target)
 
     for f in os.listdir(source):
         if os.path.isdir(source+"/"+f):
-            recursiveSounds(source+"/"+f, target+"/"+f)
+            convertSounds(source+"/"+f, target+"/"+f)
         else:
             print(source+"/"+f)
             targetFile = os.path.splitext(target+"/"+f)[0] + ".wav"
 
             subprocess.Popen("ffmpeg -hide_banner -loglevel error -i \"%s\" -acodec pcm_s16le -ar 32000 -ac 1 -b:a 96k -y \"%s\"" % (source+"/"+f, targetFile)).wait()
 
-recursiveSounds(folder+"/sounds/blips", "converted/data/ao-nds/sounds/blips")
-recursiveSounds(folder+"/sounds/general", "converted/data/ao-nds/sounds/general")
-
-
-print("\nConverting music...")
-def recursiveMusic(source, target):
+def convertMusic(source, target):
     if not os.path.exists(target):
         os.mkdir(target)
 
     for f in os.listdir(source):
         if os.path.isdir(source+"/"+f):
-            recursiveMusic(source+"/"+f, target+"/"+f)
+            convertMusic(source+"/"+f, target+"/"+f)
         else:
             print(source+"/"+f)
             targetFile = os.path.splitext(target+"/"+f)[0] + ".mp3"
@@ -371,11 +318,7 @@ def recursiveMusic(source, target):
             # need to apply this metadata title so that the mp3 player used in the NDS app doesn't act funky when loading it
             subprocess.Popen("ffmpeg -hide_banner -loglevel error -i \"%s\" -map 0:a -ar 22050 -ac 2 -b:a 96k -metadata title=\"000000000000000000000000000000000000000\" -y \"%s\"" % (source+"/"+f, targetFile)).wait()
 
-recursiveMusic(folder+"/sounds/music", "converted/data/ao-nds/sounds/music")
-
-
-print("\nConverting objection images and chatbox...")
-def chatbox():
+def convertChatbox(folder):
     print(folder+"/misc/default/chatbox.png")
     img = Image.open(folder+"/misc/default/chatbox.png").convert("RGBA")
     img = img.crop((0, -2, img.size[0], img.size[1]))
@@ -402,7 +345,7 @@ def chatbox():
     os.rename("temp.map.bin", "converted/data/ao-nds/misc/chatbox.map.bin")
     os.rename("temp.pal.bin", "converted/data/ao-nds/misc/chatbox.pal.bin")
 
-def shout(filename):
+def convertShout(filename):
     print(filename)
 
     img = Image.open(filename)
@@ -433,19 +376,3 @@ def shout(filename):
     os.rename("temp.img.bin", newFile+".img.bin")
     os.rename("temp.map.bin", newFile+".map.bin")
     os.rename("temp.pal.bin", newFile+".pal.bin")
-    
-chatbox()
-shout(folder+"/misc/default/objection_bubble.gif")
-shout(folder+"/misc/default/holdit_bubble.gif")
-shout(folder+"/misc/default/takethat_bubble.gif")
-
-print("Converting placeholder.gif...")
-convertEmoteGIF(folder+"/themes/default/placeholder.gif", "converted/data/ao-nds/misc/placeholder.gif", "converted/data/ao-nds/misc", "")
-
-
-print("Cleaning up temporary files...")
-os.remove("temp.png") # lmao
-
-print("Done!")
-print("Inside the folder named 'converted', you will see a folder named 'data'.")
-print("Copy this 'data' folder to the root of your SD card.")
---
+++ converter/main.py
@@ -0,0 +1,137 @@
+import os
+import subprocess
+
+import requests
+
+import conversion
+
+if os.name == "nt" and not os.path.exists("ffmpeg.exe"):
+    print("Downloading 7zr...")
+    a = requests.get("https://www.7-zip.org/a/7zr.exe")
+    open("7zr.exe", "wb").write(a.content)
+    
+    print("Downloading ffmpeg...")
+    with requests.get("https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z", stream=True) as r:
+        r.raise_for_status()
+        downloaded = 0
+        with open("ffmpeg.7z", 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                f.write(chunk)
+                downloaded += len(chunk)
+                print("    %.2f MB / %.2f MB" % (downloaded/1024./1024., int(r.headers["Content-length"])/1024./1024.), end="\r")
+
+    print("Extracting ffmpeg...      ")
+    subprocess.Popen("7zr e ffmpeg.7z -o. ffmpeg.exe -r").wait()
+
+    print("Extracted\n\n")
+    os.remove("ffmpeg.7z")
+    os.remove("7zr.exe")
+
+folder = ""
+while folder == "":
+    print("Drag the AO base folder to this console window then press ENTER:")
+    folder = input("> ")
+    if not os.path.exists(folder+"/characters"):
+        print("AO data does not exist there.")
+        folder = ""
+
+try:
+    os.makedirs("converted/data/ao-nds")
+    os.makedirs("converted/data/ao-nds/background")
+    os.makedirs("converted/data/ao-nds/characters")
+    os.makedirs("converted/data/ao-nds/evidence")
+    os.makedirs("converted/data/ao-nds/sounds/general")
+    os.makedirs("converted/data/ao-nds/sounds/music")
+    os.makedirs("converted/data/ao-nds/sounds/blips")
+    os.makedirs("converted/data/ao-nds/misc")
+except:
+    pass
+
+
+while 1:
+    os.system("cls||clear")
+
+    print("AO NDS converter tool")
+    print("This tool will convert your AO 'base' folder to formats that will work with the Nintendo DS.\n")
+
+    print("Make a decision:")
+    print("  1. Convert everything")
+    print("  2. Convert backgrounds")
+    print("  3. Convert characters")
+    print("  4. Convert evidence images")
+    print("  5. Convert sounds and blips")
+    print("  6. Convert music")
+    print("  7. Convert misc folder")
+    print("  8. Exit")
+
+    option = input("> ")
+    if not option.isdigit(): continue
+    option = int(option)
+    if option < 1 or option > 8: continue
+
+    if option == 1:
+        print("Converting backgrounds...")
+        conversion.convertBackgrounds(folder)
+
+        print("\nConverting characters...")
+        conversion.convertCharacters(folder+"/characters", "converted/data/ao-nds/characters")
+
+        print("\nConverting evidence images...")
+        conversion.convertEvidenceImages(folder)
+
+        print("\nConverting sounds...")
+        conversion.convertSounds(folder+"/sounds/blips", "converted/data/ao-nds/sounds/blips")
+        conversion.convertSounds(folder+"/sounds/general", "converted/data/ao-nds/sounds/general")
+
+        print("\nConverting music...")
+        conversion.convertMusic(folder+"/sounds/music", "converted/data/ao-nds/sounds/music")
+
+        print("\nConverting objection images and chatbox...")
+        conversion.convertChatbox(folder)
+        conversion.convertShout(folder+"/misc/default/objection_bubble.gif")
+        conversion.convertShout(folder+"/misc/default/holdit_bubble.gif")
+        conversion.convertShout(folder+"/misc/default/takethat_bubble.gif")
+
+        print("Converting placeholder.gif...")
+        conversion.convertEmoteGIF(folder+"/themes/default/placeholder.gif", "converted/data/ao-nds/misc/placeholder.gif", "converted/data/ao-nds/misc", "")
+
+        print("Cleaning up temporary files...")
+        if os.path.exists("temp.png"): os.remove("temp.png")
+
+        print("Done!")
+        print("Inside the folder named 'converted', you will see a folder named 'data'.")
+        print("Copy this 'data' folder to the root of your SD card.")
+        print("Press enter to return to the main menu...")
+        input()
+
+    elif option == 2:
+        conversion.convertBackgrounds(folder)
+        if os.path.exists("temp.png"): os.remove("temp.png")
+
+    elif option == 3:
+        conversion.convertCharacters(folder+"/characters", "converted/data/ao-nds/characters")
+        if os.path.exists("temp.png"): os.remove("temp.png")
+
+    elif option == 4:
+        conversion.convertEvidenceImages(folder)
+        if os.path.exists("temp.png"): os.remove("temp.png")
+
+    elif option == 5:
+        conversion.convertSounds(folder+"/sounds/blips", "converted/data/ao-nds/sounds/blips")
+        conversion.convertSounds(folder+"/sounds/general", "converted/data/ao-nds/sounds/general")
+        if os.path.exists("temp.png"): os.remove("temp.png")
+
+    elif option == 6:
+        conversion.convertMusic(folder+"/sounds/music", "converted/data/ao-nds/sounds/music")
+        if os.path.exists("temp.png"): os.remove("temp.png")
+
+    elif option == 7:
+        conversion.convertChatbox(folder)
+        conversion.convertShout(folder+"/misc/default/objection_bubble.gif")
+        conversion.convertShout(folder+"/misc/default/holdit_bubble.gif")
+        conversion.convertShout(folder+"/misc/default/takethat_bubble.gif")
+        conversion.convertEmoteGIF(folder+"/themes/default/placeholder.gif", "converted/data/ao-nds/misc/placeholder.gif", "converted/data/ao-nds/misc", "")
+        if os.path.exists("temp.png"): os.remove("temp.png")
+
+    elif option == 8:
+        break
