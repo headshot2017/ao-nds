01a679d555ddc6dfa5d8c04a255441e4ab9d42f2
headshot2017 <>
2023-10-01 22:17:38 -0400

    add OK button to disconnected screen

--- arm9/source/ui/uidisconnected.cpp
+++ arm9/source/ui/uidisconnected.cpp
@@ -8,7 +8,7 @@
 #include "engine.h"
 #include "ui/uiwificonnect.h"
 #include "ui/uimainmenu.h"
-//#include "spr_ok.h"
+#include "spr_ok.h"
 #include "bg_title.h"
 #include "bg_logo.h"
 
@@ -24,7 +24,7 @@ UIScreenDisconnected::~UIScreenDisconnected()
 
 	delete lbl_disconnectMsg;
 	delete lbl_reason;
-	//delete btn_ok;
+	delete btn_ok;
 	delete[] sndSelect;
 }
 
@@ -45,23 +45,26 @@ void UIScreenDisconnected::init()
 
 	lbl_disconnectMsg = new UILabel(&oamSub, 0, 8, 1, RGB15(31,31,31), 0, 1);
 	lbl_reason = new UILabel(&oamSub, lbl_disconnectMsg->nextOamInd(), 8, 6, RGB15(31,31,31), 0, 0);
-	//btn_ok = new UIButton(&oamSub, (u8*)spr_okTiles, (u8*)spr_okPal, lbl_reason->nextOamInd(), 2, 1, SpriteSize_32x32, 128-16, 160, 64, 32, 32, 32, 2);
+	btn_ok = new UIButton(&oamSub, (u8*)spr_okTiles, (u8*)spr_okPal, lbl_reason->nextOamInd(), 3, 1, SpriteSize_32x32, 128-(76/2), 160, 76, 26, 32, 32, 1);
 
 	lbl_disconnectMsg->setText(disconnectMsg.c_str());
 	lbl_disconnectMsg->setPos(128, 16, true);
 	lbl_reason->setText(reason.c_str());
 	lbl_reason->setPos(128, 36, true);
-	//btn_ok->connect(onOK, this);
+
+	btn_ok->assignKey(KEY_A);
+	btn_ok->connect(onOK, this);
 }
 
 void UIScreenDisconnected::updateInput()
 {
-	//btn_ok->updateInput();
+	btn_ok->updateInput();
 }
 
 void UIScreenDisconnected::onOK(void* pUserData)
 {
 	UIScreenDisconnected* pSelf = (UIScreenDisconnected*)pUserData;
+	soundPlaySample(pSelf->sndSelect, SoundFormat_16Bit, pSelf->sndSelectSize, 32000, 127, 64, false, 0);
 
 	if (pSelf->goToWiFi)
 		gEngine->changeScreen(new UIScreenWifi);
