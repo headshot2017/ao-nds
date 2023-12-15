657a356af92577395510e6906b22619d3a6e1d9f
headshot2017 <>
2023-12-14 21:26:06 -0400

    fix apng reader

--- converter/images.py
+++ converter/images.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 from apng import APNG
 import io
 
@@ -10,50 +10,54 @@ APNG_BLEND_OP_OVER = 1
 disposes = ["APNG_DISPOSE_OP_NONE", "APNG_DISPOSE_OP_BACKGROUND", "APNG_DISPOSE_OP_PREVIOUS"]
 blends = ["APNG_BLEND_OP_SOURCE", "APNG_BLEND_OP_OVER"]
 
-# layman's terms of above apng constants so i can easily wrap my head around it:
+# https://wiki.mozilla.org/APNG_Specification#.60fcTL.60:_The_Frame_Control_Chunk
 #
-# APNG_DISPOSE_OP_NONE: do nothing with next frame, keep it as it is
-# APNG_DISPOSE_OP_BACKGROUND: turn the current frame into full transparency before pasting the next frame
-# APNG_DISPOSE_OP_PREVIOUS: grab the previous frame, and paste the next one into THAT previous frame
+# `dispose_op` specifies how the output buffer should be changed at the end of the delay (before rendering the next frame).
+# APNG_DISPOSE_OP_NONE: no disposal is done on this frame before rendering the next; the contents of the output buffer are left as is.
+# APNG_DISPOSE_OP_BACKGROUND: the frame's region of the output buffer is to be cleared to fully transparent black before rendering the next frame.
+# APNG_DISPOSE_OP_PREVIOUS: the frame's region of the output buffer is to be reverted to the previous contents before rendering the next frame.
+# If the first `fcTL` chunk uses a `dispose_op` of APNG_DISPOSE_OP_PREVIOUS it should be treated as APNG_DISPOSE_OP_BACKGROUND.
 #
+# `blend_op` specifies whether the frame is to be alpha blended into the current output buffer content, or whether it should completely replace its region in the output buffer.
 # APNG_BLEND_OP_SOURCE: replace frame region
 # APNG_BLEND_OP_OVER: blend with frame
 
-def load_apng(file): # this one was hell to implement compared to the three funcs below
+def load_apng(file):
     img = APNG.open(file)
     pilframes = []
-    dispose_op = 0
     
     width, height = img.frames[0][0].width, img.frames[0][0].height
     outputbuf = Image.new("RGBA", (width, height), (255,255,255,0))
     prev_frame = None
+    prev_frame_info = None
 
     for frame, frame_info in img.frames:
         i = img.frames.index((frame, frame_info))
+        if not frame_info:
+            print(file)
+
+        if prev_frame_info and prev_frame_info.depose_op == APNG_DISPOSE_OP_BACKGROUND:
+            draw = ImageDraw.Draw(outputbuf)
+            draw.rectangle((prev_frame_info.x_offset, prev_frame_info.y_offset, prev_frame_info.x_offset+prev_frame_info.width, prev_frame_info.y_offset+prev_frame_info.height), fill=(0, 0, 0, 0))
+        elif prev_frame_info and prev_frame_info.depose_op == APNG_DISPOSE_OP_PREVIOUS:
+            outputbuf.paste(prev_frame, (prev_frame_info.x_offset, prev_frame_info.y_offset))
+
         pilframe = Image.open(io.BytesIO(frame.to_bytes())).convert("RGBA")
+        prev_frame_info = frame_info
 
-        if dispose_op == APNG_DISPOSE_OP_BACKGROUND or (i == 0 and dispose_op == APNG_DISPOSE_OP_PREVIOUS):
-            prev_frame = outputbuf.copy()
-            emptyrect = Image.new("RGBA", (img.frames[i-1][0].width, img.frames[i-1][0].height), (255,255,255,0))
-            outputbuf.paste(emptyrect, (img.frames[i-1][1].x_offset, img.frames[i-1][1].y_offset))
-
-        elif dispose_op == APNG_DISPOSE_OP_PREVIOUS:
-            outputbuf = prev_frame.copy()
+        if frame_info:
+            if frame_info.depose_op == APNG_DISPOSE_OP_PREVIOUS:
+                prev_frame = outputbuf.crop((frame_info.x_offset, frame_info.y_offset, frame_info.x_offset+frame_info.width, frame_info.y_offset+frame_info.height))
+            if frame_info.blend_op == APNG_BLEND_OP_SOURCE:
+                draw = ImageDraw.Draw(outputbuf)
+                draw.rectangle((frame_info.x_offset, frame_info.y_offset, frame_info.x_offset+frame_info.width, frame_info.y_offset+frame_info.height), fill=(0, 0, 0, 0))
         
-        else:
-            prev_frame = outputbuf.copy()
+        outputbuf.paste(pilframe, (frame_info.x_offset, frame_info.y_offset) if frame_info else (0,0), pilframe)
 
         if frame_info:
-            outputbuf.paste(pilframe, (frame_info.x_offset, frame_info.y_offset), pilframe.convert("RGBA") if frame_info.blend_op == APNG_BLEND_OP_OVER else None)
+            pilframes.append([outputbuf.copy(), int(frame_info.delay / float(frame_info.delay_den) * 1000)]) # convert delay to milliseconds
         else:
-            outputbuf.paste(pilframe, (0, 0))
-
-        final_frame = outputbuf.copy()
-        if frame_info:
-            pilframes.append([final_frame, int(frame_info.delay / float(frame_info.delay_den) * 1000)]) # convert delay to milliseconds
-            dispose_op = frame_info.depose_op
-        else:
-            pilframes.append([final_frame, 0])
+            pilframes.append([outputbuf.copy(), 0])
 
     return pilframes
 
@@ -73,7 +77,7 @@ def get_apng_duration(file):
     dur = 0
 
     for frame, frame_info in img.frames:
-        if frame_info: dur += frame_info.delay*10 # convert delay from centiseconds to milliseconds
+        if frame_info: dur += int(frame_info.delay / float(frame_info.delay_den) * 1000) # convert delay to milliseconds
 
     return dur
 
