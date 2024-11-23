b1a48cbd4625bf58674e371d66c16cb140049bfa
headshot2017 <>
2024-11-23 03:14:27 -0400

    show desk by default

--- arm9/source/ui/uicourt.cpp
+++ arm9/source/ui/uicourt.cpp
@@ -565,7 +565,7 @@ void UIScreenCourt::onMessagePV(void* pUserData, std::string msg)
 			std::string emoteModStr = argumentAt(ini["emotions"][I], 3);
 			int emoteModifier = (!emoteModStr.empty()) ? std::stoi(emoteModStr) : 0;
 			std::string deskModStr = argumentAt(ini["emotions"][I], 4);
-			int deskMod = (!deskModStr.empty()) ? std::stoi(deskModStr) : 0;
+			int deskMod = (!deskModStr.empty()) ? std::stoi(deskModStr) : 1;
 
 			std::string sound = (ini["soundn"].has(I)) ? ini["soundn"][I] : "1";
 
